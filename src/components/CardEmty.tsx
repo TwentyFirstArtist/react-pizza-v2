1460f8634e6659f8e0cfa26ebcf3935dc294770e
Pavlo <101452449+Pavl0lvaP@users.noreply.github.com>
2023-03-09 12:08:37 +0200

    clear code, add img empty card

--- src/components/CardEmty.tsx
+++ src/components/CardEmty.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import EmptyImg from '../assets/img/empty-cart.png';
 
 const cardEmty: React.FC = () => {
     return (
@@ -11,9 +12,9 @@ const cardEmty: React.FC = () => {
                         Скоріш за все ви ще не замовляли піццу.<br />
                         Для того щоб, замовити піццу перейдіть на головну сторінку.
                     </p>
-                    <img src="empty-cart.png" alt="Empty cart" />
+                    <img src={EmptyImg} alt="Empty cart" />
                     <Link to="/" className="button button--black">
-                        <span>Повернуться назад</span>
+                        <span>Повернуть назад</span>
                     </Link>
                 </div>
             </div>
